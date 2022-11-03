e41faf12ee7c1a444933c7eb336c0e9819939277
HP <HP@DESKTOP-GJGD45P>
2022-11-03 10:03:26 +0530

    First commit-1

---
+++ src/demo1/test1.java
@@ -0,0 +1,18 @@
+package demo1;
+
+public class test1 {
+
+	
+	public static void main(String[] args) {
+		
+	System.out.println("good morning");	
+		m1();
+	
+	}
+	
+	public static void m1()
+	{
+		
+		System.out.println("hello");
+	}
+}
